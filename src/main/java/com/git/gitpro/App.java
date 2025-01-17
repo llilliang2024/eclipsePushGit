65560fe693c2d11c2c6dfc756326e6429d9d1e92
llilliang <794468538@qq.com>
2025-01-17 21:01:05 +0900

    主线添加

--- src/main/java/com/git/gitpro/App.java
+++ src/main/java/com/git/gitpro/App.java
@@ -13,5 +13,6 @@ public class App
         System.out.println( "更新2" );
         System.out.println( "更新3" );
         System.out.println( "创建分支develop" );
+        System.out.println( "主干添加" );
     }
 }
